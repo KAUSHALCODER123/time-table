5c28f009fbe4949c5ae5d94adb9ee5d16aaa0115
KAUSHALCODER123 <mr.kaushal78@gmail.com>
2026-03-20 23:40:02 +0530

    fix: remove accidentally tracked .vercel folder to stop Vercel from deploying old Astro ghosts

--- .vercel/output/functions/_render.func/dist/server/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_C2zAsHch.mjs';
-export { renderers } from '../renderers.mjs';
--- .vercel/output/static/_astro/TimetableView.0akcUCpN.js
+++
@@ -1,1 +0,0 @@
-import{T as p}from"./TimetableView.CnukoiMf.js";import"./jsx-runtime.D_zvdyIk.js";import"./index.DiEladB3.js";export{p as default};
